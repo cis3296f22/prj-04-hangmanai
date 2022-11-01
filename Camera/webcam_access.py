f6512aea0e9b6eb697babeb764cb983863d3e79e
Timothy Brennan <tuk12915@temple.edu>
2022-11-01 19:33:26 -0400

    gets live feed from webcam uses q to exit

---
+++ Camera/webcam_access.py
@@ -0,0 +1,42 @@
+import cv2
+from matplotlib import pyplot as plt
+
+
+# takes a picture from the webcam with the id == 0
+def take_pic():
+    capture = cv2.VideoCapture(0)
+    ret, frame = capture.read()
+    cv2.imwrite('picture.png', frame)
+    capture.release()
+
+
+# finds the preferred camera the user wants
+# figure it out later
+def determine_camera():
+    cam_id = 0
+    return cam_id
+
+
+# starts webcam capture video
+def start_capture(cam_id):
+    capture = cv2.VideoCapture(cam_id)
+
+    while capture.isOpened():
+        ret, frame = capture.read()
+
+        cv2.imshow('Hangman text', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    capture.release()
+    cv2.destroyAllWindows()
+
+
+start_capture(0)
+# captures a frame from the camera with the id == 0
+# should loop to see which camera the user wants to use?
+# capture = cv2.VideoCapture(0)
+
+# ret is a boolean if a frame was captured
+# frame data from the webcam
+# ret, frame = capture.read()
